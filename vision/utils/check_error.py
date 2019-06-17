c6065cd5cf8f0d92a2eb67ae8579537b63110172
Roger Granada <roger.leitzke@gmail.com>
2019-06-16 21:53:28 -0300

    Verify errors in images

--- vision/utils/check_error.py
+++ vision/utils/check_error.py
@@ -7,6 +7,9 @@ import cv2
 import numpy as np
 from os.path import join, splitext, basename, dirname
 from os.path import realpath, abspath, exists, isdir, isfile
+from scipy import spatial
+from matplotlib import pyplot as plt
+import math
 
 import logging
 logger = logging.getLogger(__name__)
@@ -14,8 +17,130 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 
 from progressbar import ProgressBar
 
+def identify_green(img):
+    #img = cv2.imread(fimage)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    green_min = np.array([30, 100, 100], dtype="uint8")
+    green_max = np.array([90, 255, 255], dtype="uint8")
+    mask = cv2.inRange(hsv, green_min, green_max)
+    nb_nz = np.count_nonzero(mask)
+    perc = float(nb_nz)/(mask.shape[0]*mask.shape[1])
+    if perc > 0.5:
+        return True
+    return False
+
+def identify_red(img):
+    #img = cv2.imread(fimage)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    red_min = np.array([5, 50, 200], dtype="uint8")
+    red_max = np.array([30, 255, 255], dtype="uint8")
+    mask = cv2.inRange(hsv, red_min, red_max)
+    mask = mask[:200] # only sky
+    nb_nz = np.count_nonzero(mask)
+    if nb_nz > 10000:
+        return True
+    return False
+
+def error_channel(fimage):
+    img = cv2.imread(fimage)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    #green_min = np.array([30, 100, 100], dtype="uint8")
+    #green_max = np.array([90, 255, 255], dtype="uint8")
+    green_min = np.array([5, 50, 200], dtype="uint8")
+    green_max = np.array([30, 255, 255], dtype="uint8")
+    mask = cv2.inRange(hsv, green_min, green_max)
+    #print mask.shape
+    mask = mask[:200]
+    #print mask.shape
+    nb_nz = np.count_nonzero(mask)
+    perc = float(nb_nz)/(mask.shape[0]*mask.shape[1])
+    print perc
+    print nb_nz
+    plt.imshow(mask, cmap='gray')
+    plt.show()
+
+def __error_channel(fimage):
+    #f = plt.figure(1)
+    color = ('b','g','r')
+    img = cv2.imread(fimage)
+    hist_channel = []
+    for channel, col in enumerate(color):
+        hist = cv2.calcHist([img],[channel],None,[256],[0,256])
+        #plt.xlim([0,256])
+        #plt.plot(hist, color=col)
+        hist = hist.reshape(1, 256)
+        hist_channel.append(list(hist[0]))
+
+    #print len(hist_channel)
+
+    #calculate distance
+    #hist_b = np.array(hist_channel[0])[250:]
+    #hist_g = np.array(hist_channel[1])[250:]
+    #hist_r = np.array(hist_channel[2])[250:]
+    
+    vb = hist_channel[0][-1]
+    vg = hist_channel[1][-1]
+    vr = hist_channel[2][-1]
+
+    #dist_rg = np.linalg.norm(hist_r - hist_g)
+    #dist_rb = np.linalg.norm(hist_r - hist_b)
+    #dist_gb = np.linalg.norm(hist_g - hist_b)
+
+    #dist_rg = cosine_similarity(hist_r, hist_g)
+    #dist_rb = cosine_similarity(hist_r, hist_b)
+    #dist_gb = cosine_similarity(hist_g, hist_b)
+    
+    #plt.show()
+    total = vr+vg+vb
+
+    pr = vr/float(total)
+    pg = vg/float(total)
+    pb = vb/float(total)
+
+    print pr+pg
+    print pr
+    print pg
+    print pb
+    #print 'B', vb/float(total)
+
+    if pr+pg > 0.9 or pg < 0.1:
+        return True
+    #if dist_rg < 0.9: # or dist_rb < 0.8 or dist_gb < 0.8:
+    #    return True
+    #raw_input()
+    return False
 
 def verify_errors(filein, im_true, im_error, fileout):
+    fout = open(fileout, 'w')
+
+    logger.info('Checking number of images...')
+    for nb_imgs, _ in enumerate(open(filein), start=1): pass
+    logger.info('Input folder containing %d images' % nb_imgs)
+
+    count = 0
+    pb = ProgressBar(nb_imgs)
+    with open(filein) as fin:
+        for line in fin:
+            pb.update()
+            line = line.strip()
+            if not isfile(line):
+                continue
+
+            img = cv2.imread(line)
+            if identify_green(img) or identify_red(img):
+                fout.write('%s\n' % line)
+                count += 1
+
+    print '%d files containing errors' % count
+    fout.close()
+            
+
+
+
+def green_error(filein, im_true, im_error, fileout):
     color = ('b','g','r')
 
     fout = open(fileout, 'w')
@@ -92,7 +217,7 @@ def create_file_paths(inputfolder, fileoutput):
         if ext == '.jpg':
             names.append(int(name))
     for name in sorted(names):
-        fout.write('%s%d.jpg\n' % (inputfolder, name))
+        fout.write('%s\n' % join(inputfolder, str(name)+'.jpg'))
     print 'Saved paths in file: %s' % fileoutput
 
 
@@ -108,6 +233,8 @@ if __name__== "__main__":
     parser.add_argument('outputfile', metavar='fileout', 
                         help='Path to file to save images with error.')
     args = parser.parse_args()
+    
+    #error_channel(args.inputfile)
     
     input = args.inputfile
     im_true = args.image_true
