4d20605355a7a08d1593af0a5c7fee4d3adc792f
roger <roger.leitzke@gmail.com>
2019-08-05 14:28:16 -0300

    Improved detection of errors in images

--- vision/utils/check_error.py
+++ vision/utils/check_error.py
@@ -12,6 +12,7 @@ from os.path import realpath, isdir, isfile
 
 from progressbar import ProgressBar
 from utils import count_lines, create_paths
+import move_error_files
 
 def identify_green(img):
     """ Identify images containing errors in green and purple """
@@ -35,9 +36,34 @@ def identify_red(img):
     red_min = np.array([5, 50, 200], dtype="uint8")
     red_max = np.array([30, 255, 255], dtype="uint8")
     mask = cv2.inRange(hsv, red_min, red_max)
-    mask = mask[:200] # only sky
+    mask = mask[:100] # only sky
     nb_nz = np.count_nonzero(mask)
     if nb_nz > 10000:
+        return True
+    return False
+
+
+def identify_topline(img, size=100):
+    """ Identify images containing errors in red and green """
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    _, nb_c, _ = hsv.shape
+    mid = int(nb_c/2)
+    lo_border = mid - size
+    hi_border = mid + size
+
+    green_min = np.array([30, 100, 100], dtype="uint8")
+    green_max = np.array([90, 255, 255], dtype="uint8")
+    mask_green = cv2.inRange(hsv, green_min, green_max)
+    vec_green = mask_green[0,lo_border:hi_border]
+
+    red_min = np.array([161, 55, 84], dtype="uint8")
+    red_max = np.array([179, 255, 255], dtype="uint8")
+    mask_red = cv2.inRange(hsv, red_min, red_max)
+    vec_red = mask_red[0,lo_border:hi_border]
+    mask = vec_green + vec_red
+    nb_nz = np.count_nonzero(mask)
+    perc = float(nb_nz)/len(mask)
+    if perc > .3:
         return True
     return False
 
@@ -59,7 +85,7 @@ def verify_errors(filein, fileout):
                 continue
 
             img = cv2.imread(line)
-            if identify_green(img) or identify_red(img):
+            if identify_green(img) or identify_red(img) or identify_topline(img):
                 fout.write('%s\n' % line)
                 count += 1
     logger.info('{} files containing errors'.format(count))
@@ -70,20 +96,33 @@ def verify_errors(filein, fileout):
 if __name__== "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('inputfile', metavar='file_input', 
-                        help='file or folder containing images.')
-    parser.add_argument('outputfile', metavar='fileout', 
-                        help='Path to file to save images with error.')
+    parser.add_argument('finput', metavar='input', 
+                        help='Path to a file or folder containing images.')
+    parser.add_argument('foutput', metavar='output', 
+                        help='Path to a file or folder to save images with error.')
+    parser.add_argument('-m', '--move', action='store_true', help='Move files in case of output is a folder.' )
     args = parser.parse_args()
     
-    input = args.inputfile
+    input = args.finput
     if isdir(input):
         input = input+'/'
         dirin = dirname(realpath(input))
-        outputfile = join(dirin, 'paths.txt')
-        create_paths(input, outputfile)
-    elif isfile(input):
-        outputfile = input    
+        inputfile = join(dirin, 'paths.txt')
+        create_paths(input, inputfile)
+        input = inputfile
+
+    output = args.foutput
+    if isdir(output):
+        output = output+'/'
+        dirout = dirname(realpath(output))
+        outputfile = join(dirout, 'error.txt')
+        output = outputfile 
     
-    verify_errors(input, args.outputfile)
+    verify_errors(input, output)
+
+    if isdir(args.foutput):
+        if args.move:
+            move_error_files.move_files(output, args.foutput, False)
+        else:
+            move_error_files.move_files(output, args.foutput, True)
     
